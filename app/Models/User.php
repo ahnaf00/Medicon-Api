4b4c68bbe2044f32397bb685f2164175655b3b56
Ahnaf Anan <aanana0171@gmail.com>
2026-08-04 12:02:39 +0600

    User model updated and eloquent relationships implemented

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,26 +7,78 @@ use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Spatie\Permission\Traits\HasRoles;
 
 #[Fillable(['name', 'email', 'password'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasRoles;
 
     /**
      * Get the attributes that should be cast.
      *
      * @return array<string, string>
      */
+    protected $fillable = [
+        'name','email','phone','password','status'
+    ];
+
+    protected $hidden = [
+        'password','remember_token'
+    ];
+
+
     protected function casts(): array
     {
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function patientProfile():HasOne
+    {
+        return $this->hasOne(PatientProfile::class, 'user_id');
+    }
+
+    public function doctorProfile():HasOne
+    {
+        return $this->hasOne(DoctorProfile::class,'user_id');
+    }
+
+    public function appointmentsAsPatient():HasMany
+    {
+        return $this->hasMany(Appointment::class,'patient_user_id');
+    }
+
+    public function appointmentsAsDoctor():HasMany
+    {
+        return $this->hasMany(Appointment::class,'doctor_user_id');
+    }
+
+    public function PrescriptionsAsPatient():HasMany
+    {
+        return $this->hasMany(Prescription::class, 'patient_user_id');
+    }
+
+    public function PrescriptionsAsDoctor():HasMany
+    {
+        return $this->hasMany(Prescription::class,'doctor_user_id');
+    }
+
+    public function medicalRecords():HasMany
+    {
+        return $this->hasMany(MedicalRecord::class,'patient_user_id');
+    }
+
+    public function billings():HasMany
+    {
+        return $this->hasMany(Billing::class,'patient_user_id');
     }
 }
