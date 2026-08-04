9c30541dfc519dc51d3f55dd662d5405ed49c607
Ahnaf Anan <aanana0171@gmail.com>
2026-08-04 12:44:57 +0600

    Appointment model updated and eloquent relationship implemented

---
+++ app/Models/Appointment.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
+class Appointment extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'patient_user_id',
+        'doctor_user_id',
+        'appointment_datetime',
+        'format',
+        'status',
+        'notes',
+    ];
+
+     protected $casts = [
+        'appointment_datetime' => 'datetime',
+    ];
+
+    public function patient():BelongsTo
+    {
+        return $this->belongsTo(User::class,'patient_user_id');
+    }
+
+    public function doctor():BelongsTo
+    {
+        return $this->belongsTo(User::class,'doctor_user_id');
+    }
+
+    public function prescription():HasOne
+    {
+        return $this->hasOne(Prescription::class,'appointment_id');
+    }
+
+    public function billing():HasOne
+    {
+        return $this->hasOne(Billing::class,'appointment_id');
+    }
+}
